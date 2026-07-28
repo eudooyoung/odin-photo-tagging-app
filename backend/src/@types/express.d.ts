ac5d594158747e0107258752c0523bbe8ff97d14
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-07-28 22:37:07 +0900

    remove: unnecessary packages

--- backend/src/@types/express.d.ts
+++
@@ -1,9 +0,0 @@
-import type { AuthUser } from "@/types/auth.types.js";
-
-declare global {
-  namespace Express {
-    interface User extends AuthUser {}
-  }
-}
-
-export {};
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,24 +1,16 @@
 import express from "express";
 import errorHandler from "./errors/errorHandler.js";
-import path from "node:path";
 import flash from "express-flash";
 import { session } from "./config/session.config.js";
-import { passport } from "./config/passport.config.js";
 
 export const createApp = () => {
   const app = express();
 
   app
-    .set("views", path.join(import.meta.dirname, "views"))
-    .set("view engine", "ejs")
-
-    .use(express.static(path.join(import.meta.dirname, "public")))
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
-
     .use(session)
-    .use(flash())
-    .use(passport.session());
+    .use(flash());
 
   app.use(errorHandler);
 
