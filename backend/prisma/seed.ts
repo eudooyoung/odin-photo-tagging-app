2b9f4fa840b791203f015fda9ee2b075d23ff01b
Kim Doo Young <177651948+eudooyoung@users.noreply.github.com>
2026-07-29 16:00:02 +0900

    feat: start making seed.ts

---
+++ backend/prisma/seed.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../src/generated/prisma/client.js";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+async function main() {}
+
+main()
+  .then(async () => {
+    // await prisma.$transaction()
+    await prisma.$disconnect();
+    await pool.end();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    await pool.end();
+    process.exit(1);
+  });
